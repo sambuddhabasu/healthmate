d0147a8ec37d00955d200992dd41738ea2ca29c1
Sambuddha Basu <sammygamer@live.com>
2014-05-24 04:27:57 +0400

    Location search added

--- api_doctor_search.php
+++ api_doctor_search.php
@@ -23,19 +23,38 @@ curl_setopt($patient, CURLOPT_RETURNTRANSFER, true);
 $patientResponse = curl_exec($patient);
 $patientResponse = json_decode($patientResponse);
 curl_close($patient);
-$myLat = (float)$patientResponse->latitude;
-$myLng = (float)$patientResponse->longitude;
-//echo $myLat . " " . $myLng;
+
+if($_GET['near'] == "me" || $_GET['near'] == "") {
+    $myLat = (float)$patientResponse->latitude;
+    $myLng = (float)$patientResponse->longitude;
+}
+else {
+$location = curl_init();
+curl_setopt($location, CURLOPT_URL, "https://maps.googleapis.com/maps/api/geocode/json?address=" . $_GET['near']);
+curl_setopt($location, CURLOPT_CUSTOMREQUEST, "GET");
+curl_setopt($location, CURLOPT_RETURNTRANSFER, true);
+$locationResponse = curl_exec($location);
+$locationResponse = json_decode($locationResponse);
+curl_close($location);
+//print_r($locationResponse->results[0]->geometry->location);
+$myLat = (float)$locationResponse->results[0]->geometry->location->lat;
+$myLng = (float)$locationResponse->results[0]->geometry->location->lng;
+
+}
+
+
+
 $results = $response->results;
 
 function bubble_sort($arr) {
+    global $myLat, $myLng;
     $size = count($arr);
     for ($i=0; $i<$size; $i++) {
         for ($j=0; $j<$size-1-$i; $j++) {
-        	$checkLat1 = (float)$results[$j]->latitude;
-        	$checkLng1 = (float)$results[$j]->longitude;
-        	$checkLat2 = (float)$results[$j+1]->latitude;
-        	$checkLng2 = (float)$results[$j+1]->longitude;
+        	$checkLat1 = (float)$arr[$j]->latitude;
+        	$checkLng1 = (float)$arr[$j]->longitude;
+        	$checkLat2 = (float)$arr[$j+1]->latitude;
+        	$checkLng2 = (float)$arr[$j+1]->longitude;
         	$dist1 = pow($checkLat1 - $myLat, 2) + pow($checkLng1 - $myLng, 2);
         	$dist2 = pow($checkLat2 - $myLat, 2) + pow($checkLng2 - $myLng, 2);
             if($dist2 < $dist1) {
